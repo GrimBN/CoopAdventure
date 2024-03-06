ca92568d37c491ec5e04fc7ee4dbe8843646b80e
Agrim Bhasin <bhasin.agrim996@gmail.com>
2024-03-06 15:12:08 +0530

    Basic session interface stuff

--- Source/CoopAdventure/MultiplayerSessionsSubsystem.cpp
+++ Source/CoopAdventure/MultiplayerSessionsSubsystem.cpp
@@ -2,28 +2,42 @@
 
 
 #include "MultiplayerSessionsSubsystem.h"
+#include "OnlineSubsystem.h"
 
 void PrintString(const FString& StringToPrint)
 {
 	if (GEngine)
 	{
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, StringToPrint);
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, StringToPrint);
 	}
 }
 
 UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
 {
-	PrintString("MSS Constructor");
+	//PrintString("MSS Constructor");
 }
 
 void UMultiplayerSessionsSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
-	Super::Initialize(Collection);
-	PrintString("MSS Initialize");
+	//Super::Initialize(Collection);
+	//PrintString("MSS Initialize");
+	IOnlineSubsystem* OnlineSubsystem = IOnlineSubsystem::Get();
+
+	if (OnlineSubsystem)
+	{
+		PrintString(OnlineSubsystem->GetSubsystemName().ToString());
+
+		SessionInterface = OnlineSubsystem->GetSessionInterface();
+
+		if (SessionInterface.IsValid())
+		{
+			PrintString("Session Interface is valid");
+		}
+	}
 }
 
 void UMultiplayerSessionsSubsystem::Deinitialize()
 {
-	Super::Deinitialize();
-	UE_LOG(LogTemp, Warning, TEXT("MSS Deinitialize"));
+	//Super::Deinitialize();
+	//UE_LOG(LogTemp, Warning, TEXT("MSS Deinitialize"));
 }
